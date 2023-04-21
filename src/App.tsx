0f4036d532007933f4094a8a86db3e3f6da13a9f
Connor Wade <connor.wade.ut@gmail.com>
2023-04-20 23:53:08 -0600

    finished state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useCallback, useEffect, useReducer, useState } from "react";
 import "./App.css";
 import { List, Search } from "./components";
 import { ListItemProps } from "./components/ListItem";
@@ -6,55 +6,140 @@ import { Story } from "./types";
 
 const ENDPOINT = new URL(`https://hn.algolia.com/api/v1/search?query=`);
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-] satisfies Story[];
+const useSemiPersistentState = (
+  key: string,
+  initialState: string
+): [string, React.Dispatch<React.SetStateAction<string>>] => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case "STORIES_FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "STORIES_FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "STORIES_FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case "REMOVE_STORY":
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+};
+
+// const initialStories = [
+//   {
+//     title: "React",
+//     url: "https://reactjs.org/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: "Redux",
+//     url: "https://redux.js.org/",
+//     author: "Dan Abramov, Andrew Clark",
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+// ] satisfies Story[];
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [stories, setStories] = useState(initialStories);
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setSearchTerm(event.target.value);
-    console.log(searchTerm);
-  };
+  const [url, setUrl] = useState(`${ENDPOINT.toString()}${searchTerm}`);
+
+  const [stories, dispatchStories] = useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
+
+  const handleFetchStories = useCallback(async () => {
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
+
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+
+      dispatchStories({
+        type: "STORIES_FETCH_SUCCESS",
+        payload: result.hits,
+      });
+    } catch (e) {
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+      console.log("error:", e);
+    }
+  }, [url]);
+
+  useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item: Story) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-
-    setStories(newStories);
+    dispatchStories({
+      type: "REMOVE_STORY",
+      payload: item,
+    });
   };
 
-  const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleSearchInput = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: FormEvent<HTMLFormElement>) => {
+    setUrl(`${ENDPOINT.toString()}${searchTerm}`);
+    event.preventDefault();
+  };
 
   return (
     <>
       <h1>My Hacker Stories</h1>
 
-      <Search onChange={handleSearch} />
+      <Search
+        searchTerm={searchTerm}
+        onSubmit={handleSearchSubmit}
+        onChange={handleSearchInput}
+      />
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
+      )}
     </>
   );
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,28 @@
+import { FormEvent, useRef } from "react";
+
 export type SearchProps = {
+  searchTerm: string;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export default function Search({ onChange }: SearchProps) {
+export default function Search({
+  onChange,
+  searchTerm,
+  onSubmit,
+}: SearchProps) {
+  const inputRef = useRef(null);
+
   return (
-    <div>
+    <form onSubmit={onSubmit}>
       <label>
         Search:
-        <input type="text" onChange={onChange} />
+        <input type="text" ref={inputRef} onChange={onChange} />
       </label>
-    </div>
+
+      <button type="submit" disabled={!searchTerm}>
+        Submit
+      </button>
+    </form>
   );
 }
